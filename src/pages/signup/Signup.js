96c41675bf8837bbea220bdfb39a59fff66944f9
Dhruv <dev.aroradhruv@gmail.com>
2022-01-06 08:08:23 +0530

    Signup page UI changes

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 //STYLES
 import "./Signup.css";
+import signupImage from "../../assets/signup.svg";
+
 export default function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -9,40 +11,48 @@ export default function Signup() {
   const [thumbnail, setThumbnail] = useState(null);
 
   return (
-    <form className="auth-form">
-      <h2>Sign up</h2>
-      <label>
-        <span>Email:</span>
-        <input
-          required
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-      </label>
-      <label>
-        <span>Password:</span>
-        <input
-          required
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-      </label>
-      <label>
-        <span>Display Name:</span>
-        <input
-          required
-          type="text"
-          onChange={(e) => setDisplayName(e.target.value)}
-          value={displayName}
-        />
-      </label>
-      <label>
-        <span>Display Picture:</span>
-        <input required type="file" />
-      </label>
-      <button className="btn">Sign up</button>
-    </form>
+    <div class="grid-container">
+      <div class="grid-child">
+        <img src={signupImage} alt="image" />
+      </div>
+
+      <div class="grid-child">
+        <form className="auth-form">
+          <h2>Create an account.</h2>
+          <label>
+            <input
+              placeholder="Email"
+              required
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </label>
+          <label>
+            <input
+              placeholder="Password"
+              required
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </label>
+          <label>
+            <input
+              placeholder="Display Name"
+              required
+              type="text"
+              onChange={(e) => setDisplayName(e.target.value)}
+              value={displayName}
+            />
+          </label>
+          <label>
+            <span>Display Picture:</span>
+            <input required type="file" />
+          </label>
+          <button className="btn">Sign up</button>
+        </form>
+      </div>
+    </div>
   );
 }
