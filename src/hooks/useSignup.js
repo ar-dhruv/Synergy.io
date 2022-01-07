5fb28bb8b46b777013608ec11a5c02df6c3bad4c
Dhruv <dev.aroradhruv@gmail.com>
2022-01-07 07:54:24 +0530

    upload thumbnails to firebase storage

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { projectAuth } from "../firbase/config";
+import { projectAuth, projectStorage } from "../firbase/config";
 import { useAuthContext } from "./useAuthContext";
 
 export const useSignup = () => {
@@ -9,7 +9,7 @@ export const useSignup = () => {
   const { dispatch } = useAuthContext();
   const [isCancelled, setIsCancelled] = useState(false); //STATES FOR CLEAN-UP FUNCTION
 
-  const signup = async (email, password, displayName) => {
+  const signup = async (email, password, displayName, thumbnail) => {
     setError(null); //WE RESET THE ERROR TO BE NULL EVERYTIME WE TRY TO SIGNUP
     setIsPending(true);
 
@@ -25,8 +25,17 @@ export const useSignup = () => {
         throw new Error("Could not complete signup");
       }
 
-      //ADD DISPLAY NAME TO THE USER IN FIREBASE
-      await res.user.updateProfile({ displayName: displayName });
+      //UPLOAD USER THUMBNAIL TO THE FIREBASE
+      const uploadPath = `thumbnails/${res.user.uid}/${thumbnail.name}` //WE UPLAOD THE IMAGE IN A THUMBNAILS FOLDER...INSIDE THERE IS ANOTHER FOLDER WITH A NAME OF THE USER ID AND THEN INSIDE THE TUMBNAIL IMAGE FILE WITH ITS ORIGINAL NAME
+      const img = await projectStorage.ref(uploadPath).put(thumbnail)
+      const imgURL = await img.ref.getDownloadURL() //WE STORE OUR THUMBNAIL IN THE FIREBASE AT uploadPath AS img & THEN GETS ITS URL AS imgURL
+
+
+      //ADD DISPLAY NAME & DISPLAY IMAGE TO THE USER IN FIREBASE
+      await res.user.updateProfile({
+        displayName: displayName,
+        photoURL: imgURL,
+      });
 
       //DISPATCHING THE LOGIN ACTION WITH PAYLOAD AS THE RES.USER RETURNED BY THE FIRESTORE
       dispatch({ type: "LOGIN", payload: res.user });
