a594f3a0f0d8cdcea335981cf29a5728f27ec3ed
Dhruv <dev.aroradhruv@gmail.com>
2022-01-07 13:34:33 +0530

    Creating the user document

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
-import { projectAuth, projectStorage } from "../firebase/config";
+import {
+  projectAuth,
+  projectStorage,
+  projectFirestore,
+} from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
 export const useSignup = () => {
@@ -26,16 +30,22 @@ export const useSignup = () => {
       }
 
       //UPLOAD USER THUMBNAIL TO THE FIREBASE
-      const uploadPath = `thumbnails/${res.user.uid}/${thumbnail.name}` //WE UPLAOD THE IMAGE IN A THUMBNAILS FOLDER...INSIDE THERE IS ANOTHER FOLDER WITH A NAME OF THE USER ID AND THEN INSIDE THE TUMBNAIL IMAGE FILE WITH ITS ORIGINAL NAME
-      const img = await projectStorage.ref(uploadPath).put(thumbnail)
-      const imgURL = await img.ref.getDownloadURL() //WE STORE OUR THUMBNAIL IN THE FIREBASE AT uploadPath AS img & THEN GETS ITS URL AS imgURL
-
+      const uploadPath = `thumbnails/${res.user.uid}/${thumbnail.name}`; //WE UPLAOD THE IMAGE IN A THUMBNAILS FOLDER...INSIDE THERE IS ANOTHER FOLDER WITH A NAME OF THE USER ID AND THEN INSIDE THE TUMBNAIL IMAGE FILE WITH ITS ORIGINAL NAME
+      const img = await projectStorage.ref(uploadPath).put(thumbnail);
+      const imgURL = await img.ref.getDownloadURL(); //WE STORE OUR THUMBNAIL IN THE FIREBASE AT uploadPath AS img & THEN GETS ITS URL AS imgURL
 
       //ADD DISPLAY NAME & DISPLAY IMAGE TO THE USER IN FIREBASE
       await res.user.updateProfile({
         displayName: displayName,
         photoURL: imgURL,
       });
+
+      //CREATING A USER DOCUMNET & STORING IN FIRESTORE DATABASE SO THAT WE CAN HAVE ACCESS TO THE DISPLAY NAME IMAGE ETC OF A USER THAT IS NOT EVEN LOGGED IN BUT IS A PART OF THE TEAM SO THAT WE CAN DISPLAY THAT INFORMATION IN THE SIDEBAR
+      await projectFirestore.collection("users").doc(res.user.uid).set({
+        online: true,
+        displayName: displayName,
+        photoURL: imgURL,
+      }); //WE CREATE USER DOCUMENTS INSIDE THE USERS COLLECTION WHERE EACH USER DOCUMNET WILL HAVE SAME ID AS THE USER ID ITS BELONGS
 
       //DISPATCHING THE LOGIN ACTION WITH PAYLOAD AS THE RES.USER RETURNED BY THE FIRESTORE
       dispatch({ type: "LOGIN", payload: res.user });
