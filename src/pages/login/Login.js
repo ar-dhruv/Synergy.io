201f002149516094ad3d88bafd6a0b98649aa31f
Dhruv <dev.aroradhruv@gmail.com>
2022-01-08 08:09:49 +0530

    useLogin hook in login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,38 +1,46 @@
 import { useState } from "react";
+import { useLogin } from "../../hooks/useLogin";
+
 //STYLES
 import "./Login.css";
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, error, isPending } = useLogin();
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email,password);
-    }
-
-    return (
-        <form onSubmit={handleSubmit} className="auth-form">
+  return (
+    <form onSubmit={handleSubmit} className="auth-form">
       <h2>login</h2>
       <label>
         <span>email:</span>
         <input
           required
-          type="email" 
-          onChange={(e) => setEmail(e.target.value)} 
-          value={email} 
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
         />
       </label>
       <label>
         <span>password:</span>
-        <input 
+        <input
           required
-          type="password" 
-          onChange={(e) => setPassword(e.target.value)} 
-          value={password} 
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
         />
       </label>
-      <button className="btn">Log in</button>
+      {!isPending && <button className="btn">Sign up</button>}
+      {isPending && (
+        <button className="btn" disabled>
+          loading...
+        </button>
+      )}
+      {error && <div className="error">{error}</div>}
     </form>
-    )
+  );
 }
