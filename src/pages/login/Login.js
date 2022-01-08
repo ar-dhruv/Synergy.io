9245a981f1b841d3eb9eea35a7a86acf8fca4e83
Dhruv <dev.aroradhruv@gmail.com>
2022-01-08 08:30:27 +0530

    changes

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -3,6 +3,9 @@ import { useLogin } from "../../hooks/useLogin";
 
 //STYLES
 import "./Login.css";
+import loginImage from "../../assets/signup.svg";
+
+
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -14,33 +17,41 @@ export default function Login() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="auth-form">
-      <h2>login</h2>
-      <label>
-        <span>email:</span>
-        <input
-          required
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-      </label>
-      <label>
-        <span>password:</span>
-        <input
-          required
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-      </label>
-      {!isPending && <button className="btn">Sign up</button>}
-      {isPending && (
-        <button className="btn" disabled>
-          loading...
-        </button>
-      )}
-      {error && <div className="error">{error}</div>}
-    </form>
+    <div className="grid-container">
+      <div className="grid-child">
+        <img src={loginImage} alt="image" />
+      </div>
+
+      <div className="grid-child">
+        <form className="auth-form" onSubmit={handleSubmit}>
+          <h2>Create an account.</h2>
+          <label>
+            <input
+              placeholder="Email"
+              required
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </label>
+          <label>
+            <input
+              placeholder="Password"
+              required
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </label>
+          {!isPending && <button className="btn">Sign up</button>}
+          {isPending && (
+            <button className="btn" disabled>
+              loading...
+            </button>
+          )}
+          {error && <div className="error">{error}</div>}
+        </form>
+      </div>
+    </div>
   );
 }
