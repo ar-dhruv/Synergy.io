87362536acb396ad69e83baa1fd7eb35c0677926
Dhruv <dev.aroradhruv@gmail.com>
2022-01-09 08:24:47 +0530

    Conditional navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 //STYLES & LOGOS
 import "./Navbar.css";
@@ -7,6 +8,7 @@ import Molecular from "../assets/molecular.png";
 
 export default function Navbar() {
   const { logout, isPending } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <div className="navbar">
@@ -15,16 +17,32 @@ export default function Navbar() {
           <img src={Molecular} alt="Synergy Logo" />
           <span>Synergy</span>
         </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          {!isPending && <button className="btn" onClick={logout}>Logout</button>}
-          {isPending && <button className="btn" onClick={logout}>Logging out...</button>}
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li>
+              {!isPending && (
+                <button className="btn" onClick={logout}>
+                  Logout
+                </button>
+              )}
+              {isPending && (
+                <button className="btn" onClick={logout}>
+                  Logging out...
+                </button>
+              )}
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
