62e3414e0d59a52ab08bf52771e0871c8a8e2db1
Dhruv <dev.aroradhruv@gmail.com>
2022-01-11 07:39:36 +0530

    styling online users sidebar

--- src/components/OnlineUsers.js
+++ src/components/OnlineUsers.js
@@ -12,7 +12,7 @@ export default function OnlineUsers() {
       {error && <div className="error">{error}</div>}
       {documents &&
         documents.map((user) => (
-          <div key={user.id}>
+          <div key={user.id} className="user-list-item">
             <span>{user.displayName}</span>
             <Avatar src={user.photoURL} />
           </div>
