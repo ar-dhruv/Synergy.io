7a144dea63ef2b0ef16ac0ab2ff074fa45fcdde8
Dhruv <dev.aroradhruv@gmail.com>
2022-01-11 08:02:37 +0530

    using online users sidebar

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Project from "./pages/project/Project";
 import Signup from "./pages/signup/Signup";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import OnlineUsers from "./components/OnlineUsers";
 
 function App() {
   const { user, authIsReady } = useAuthContext();
@@ -46,6 +47,7 @@ function App() {
               </Route>
             </Switch>
           </div>
+          {user && <OnlineUsers/>}
         </BrowserRouter>
       )}
     </div>
