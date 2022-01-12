2a23caac90a02ecc224d1a639d058f3d199e4972
Dhruv <dev.aroradhruv@gmail.com>
2022-01-12 08:07:04 +0530

    form created

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -2,21 +2,57 @@ import { useState } from "react";
 //STYLES
 import "./Create.css";
 export default function Create() {
+  //FORM FIELDS STATES
+  const [name, setName] = useState("");
+  const [details, setDetails] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [category, setCategory] = useState("");
+  const [assignedUsers, setAssignedUsers] = useState([]);
 
-    //FORM FIELDS STATES
-    const [name,setName] = useState("");
-    const [details,setDetails] = useState("");
-    const [dueDate,setDueDate] = useState("");
-    const [category,setCategory] = useState("");
-    const [assignedUsers,setAssignedUsers] = useState([]);
+  return (
+    <div className="create-form">
+      <h2 className="page-title">Create a new Project</h2>
+      <form>
+        <label>
+          <span>Project name:</span>
+          <input
+            required
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
+        <label>
+          <span>Project details:</span>
+          <textarea
+            required
+            type="text"
+            onChange={(e) => setDetails(e.target.value)}
+            value={details}
+          ></textarea>
+        </label>
+        <label>
+          <span>Set due date:</span>
+          <input
+            required
+            type="date"
+            onChange={(e) => setDueDate(e.target.value)}
+            value={dueDate}
+          />
+        </label>
 
-    return (
-        <div className="create-form">
-            <h2 className="page-title">Create a new Project</h2>
-            <form>
+        <label>
+            <span>Project category:</span>
+            {/* category select here */}
+        </label>
+        <label>
+            <span>Assign to:</span>
+            {/* assignee select here */}
+        </label>
 
-            </form>
-        </div>
-    )
+        <button className="btn">Add Project</button>
+
+      </form>
+    </div>
+  );
 }
-
