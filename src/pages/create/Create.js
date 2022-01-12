22002eecb9ca5948a91b39f6f7ac0b68313e05c5
Dhruv <dev.aroradhruv@gmail.com>
2022-01-12 07:58:29 +0530

    create form fields states

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
 //STYLES
 import "./Create.css";
 export default function Create() {
+
+    //FORM FIELDS STATES
+    const [name,setName] = useState("");
+    const [details,setDetails] = useState("");
+    const [dueDate,setDueDate] = useState("");
+    const [category,setCategory] = useState("");
+    const [assignedUsers,setAssignedUsers] = useState([]);
+
     return (
-        <div>
-            Create
+        <div className="create-form">
+            <h2 className="page-title">Create a new Project</h2>
+            <form>
+
+            </form>
         </div>
     )
 }
