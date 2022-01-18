0e36c1abacf4307c0ce7d02b8336dc2161107b48
Dhruv <dev.aroradhruv@gmail.com>
2022-01-18 13:01:40 +0530

    fetching projects

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,11 +1,15 @@
+import { useCollection } from "../../hooks/useCollection";
+
 //STYLES
 import "./Dashboard.css";
 export default function Dashboard() {
-    return (
-        <div>
-            Dashboard
-        </div>
-    )
+  const { documents, error } = useCollection("projects");
+
+  return (
+    <div>
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && documents.map((doc) => <p key={doc.id}>{doc.name}</p>)}
+    </div>
+  );
 }
-
-
