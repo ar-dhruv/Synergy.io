ab34f65a61f1af36a0df877bee8815d89fa55d14
Dhruv <dev.aroradhruv@gmail.com>
2022-01-19 19:29:36 +0530

    useDocument hook cleanup function

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -10,12 +10,22 @@ export const useDocument = (collection, id) => {
     const ref = projectFireStore.collection(collection).doc(id); //REFERENCE TO THE DOCUMENT REQUESTED
 
     //WE FIRE THIS CALLBACK FUNTION EVERYTIME WE GET A SNAPSHOT OF THE REQUESTED DOCUMENT FROM THE FIREBASE
-    ref.onSnapshot((snapshot) => {
-      setDocument({ ...snapshot.data(), id: snapshot.id });
-      setError(null);
-    } , (error) => {
+    const unsubscribe = ref.onSnapshot(
+      (snapshot) => {
+        setDocument({ ...snapshot.data(), id: snapshot.id });
+        setError(null);
+      },
+      (error) => {
         console.log(error.message);
-        setError("Failed to Document")
-    }); //THE SECOND ARGUMENT OF THE ONSNAPSHOT METHOD IS THE ERROR IF WE DIDN'T GET BACK THE SNAPSHOT FROM THE FIREBASE
+        setError("Failed to Document");
+      }
+    ); //THE SECOND ARGUMENT OF THE ONSNAPSHOT METHOD IS THE ERROR IF WE DIDN'T GET BACK THE SNAPSHOT FROM THE FIREBASE
+
+    return () => {
+      unsubscribe();
+    }; //CLEAN-UP FUNCTION...THIS FIRES WHENEVER THE COMPONENT WHICH USES THIS HOOK UNMOUNTS
+    //AT THIS TIME WE NEED TO UNSUBSCRIBE FROM THE REALTIME DATA FOR THE DOCUMENT
   }, [collection, id]);
+
+  return { document, error };
 };
