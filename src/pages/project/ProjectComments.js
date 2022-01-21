7c31fba4fa8ffa34ba65eb5f692c9253c09edee2
Dhruv <dev.aroradhruv@gmail.com>
2022-01-21 13:09:51 +0530

    commentToAdd object

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -4,11 +4,20 @@ import { useAuthContext } from "../../hooks/useAuthContext";
 
 export default function ProjectComments() {
   const [newComment, setNewComment] = useState("");
+  const { user } = useAuthContext();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-  }
+    const commentToAdd = {
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      content: newComment,
+      createdAt: timestamp.fromDate(new Date()),
+      id: Math.random(),
+    };
+    console.log(commentToAdd);
+  };
 
   return (
     <div className="project-coments">
